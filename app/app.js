03fcb83a39fe4b6ee80a3d802af6b0e9c14da19e
Rakibul Hasan <rakibulhasan.bdm@gmail.com>
2023-08-19 15:24:49 +0600

    build directory

--- app/app.js
+++ app/app.js
@@ -16,9 +16,9 @@ app.use(errorHandler)
 
 
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (_req, res) => {
-   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
